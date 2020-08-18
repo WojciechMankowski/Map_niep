eb9f080c4597515f434a2f89f18f70bf3c4fa0f1
Wojciech Mankowski <52933584+WojciechMankowski@users.noreply.github.com>
2020-08-18 11:56:13 +0200

    Add files via upload

---
+++ function_call.py
@@ -0,0 +1,11 @@
+from Create_a_map.Create_maps import create_point_museum, create_point_cinema, create_point_sicens, \
+    create_point_green_ares, create_point_bus_stop, runing
+
+
+def run_expane():
+    create_point_bus_stop()
+    create_point_museum()
+    create_point_cinema()
+    create_point_sicens()
+    create_point_green_ares()
+    runing()
---
+++ main.py
@@ -0,0 +1,10 @@
+from function_call import run_expane
+
+
+def run_expanse():
+    print("Zaczynam pracę")
+    run_expane()
+    print("Zapisałem i kończę")
+
+if __name__ == "__main__":
+    run_expanse()
